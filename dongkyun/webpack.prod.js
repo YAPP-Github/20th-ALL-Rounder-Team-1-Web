408441f2444551fe4b58960d6a672bb3124ff3a6
miro-ring <miro.ring@kakaocorp.com>
2022-04-24 15:59:09 +0900

    feat: css 파일 최적화 플러그인 추가

--- dongkyun/webpack.prod.js
+++ dongkyun/webpack.prod.js
@@ -4,4 +4,17 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
 });
