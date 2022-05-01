863c8b35eb04030a40ae88daa67a9cc9088fcaa9
장동균 <57230590+Dongkyun-Jang@users.noreply.github.com>
2022-05-01 16:30:56 +0900

    기본 웹팩 설정 (#1)
    * feat: 웹팩 설정
    
    * feat: 바벨 설정
    
    * feat: 임시로 프리티어 설정
    
    * feat: 잡다한 파일들과 초기 파일들 업로드
    
    * chore: 웹팩 실행 명령어 변경
    
    * feat: tsconfig 파일 추가
    
    여기서 어떤 기능들을 쓰게 될지 찾아봐야 할 듯...
    
    * feat: tsconfig 설정 프리티어에서 예외 설정
    
    * chore: tsconfig 설정 수정
    
    * chore: 띄어쓰기 수정
    
    * feat: css 파일 최적화 플러그인 추가
    
    * feat: css 파일 최적화 플러그인 추가2
    
    * feat: yarn.lock 추가
    
    * feat: terser-webpack-plugin 추가
    
    * chore: css관련 loader들 위치 변경
    
    Co-authored-by: miro-ring <miro.ring@kakaocorp.com>

---
+++ dongkyun/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <div>웹팩 테스트</div>
+    </>
+  );
+};
+
+export default App;
---
+++ dongkyun/webpack.dev.js
@@ -0,0 +1,20 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    port: 8080,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+});
