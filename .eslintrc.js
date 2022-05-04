ec69d4f7e096e11a401d681e8e32a785350fc0eb
Dongkyun-Jang <jangdongkyun6576@gmail.com>
2022-05-05 00:25:16 +0900

    :sparkles: lint 설정 추가

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['prettier'],
+  extends: [
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'import/prefer-default-export': 0,
+    'prettier/prettier': 0,
+    'import/extensions': 0,
+    'no-use-before-define': 0,
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
+    'no-shadow': 0,
+    'react/prop-types': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+  },
+};
