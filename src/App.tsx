523e69e28d1e89c3085d03c87e2b45c4ccf51b5c
이수동 <tnehd1998@gmail.com>
2022-05-15 13:49:57 +0900

    :sparkles: 모든 ContextProvider를 감싸는 AppProvider 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import React from 'react';
 import TestPage from './pages/TestPage';
 import { PopUpContextProvider } from './store';
 
+interface IProps {
+  components: Array<React.JSXElementConstructor<React.PropsWithChildren<any>>>;
+  children: React.ReactNode;
+}
+
+const AppProvider = ({ components = [], children }: IProps) => {
+  return (
+    <>
+      {components.reduceRight((accumulatedComponents, CurrentComponent) => {
+        return <CurrentComponent>{accumulatedComponents}</CurrentComponent>;
+      }, children)}
+    </>
+  );
+};
+
 const App = () => {
   return (
-    <PopUpContextProvider>
+    <AppProvider components={[PopUpContextProvider]}>
       <TestPage />
-    </PopUpContextProvider>
+    </AppProvider>
   );
 };
 
