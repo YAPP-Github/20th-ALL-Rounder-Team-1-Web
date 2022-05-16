e11d06744d094b0ddba0bdc3243517e241a5da24
이수동 <tnehd1998@gmail.com>
2022-05-16 14:51:34 +0900

    :bug: 옵셔널 체이닝, PropsWithChildren 적용

--- src/utils/AppProvider.tsx
+++ src/utils/AppProvider.tsx
@@ -1,9 +1,9 @@
-interface IProps {
-  components: Array<React.JSXElementConstructor<React.PropsWithChildren<any>>>;
+interface IPropsWithChildren {
+  components?: Array<React.JSXElementConstructor<React.PropsWithChildren<any>>>;
   children: React.ReactNode;
 }
 
-export const AppProvider = ({ components = [], children }: IProps) => {
+export const AppProvider = ({ components = [], children }: IPropsWithChildren) => {
   return (
     <>
       {components.reduceRight((accumulatedComponents, CurrentComponent) => {
