b29ffec0de3c45696641ee3246005e3918f254b1
Dongkyun-Jang <jangdongkyun6576@gmail.com>
2022-05-21 13:09:22 +0900

    :bug: 순서 변경

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -3,12 +3,12 @@ import { createGlobalStyle } from 'styled-components';
 import { reset } from './reset';
 
 export const GlobalStyle = createGlobalStyle`
-   ${reset}
-
    @font-face {
       font-family: 'Pretendard';
         font-style: normal;
         src: url(https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css) format('woff2'),
         url(https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css) format('woff');
    }
+
+   ${reset}
 `;
