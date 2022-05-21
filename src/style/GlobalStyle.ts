fb5001565d950f1c52b37c5873f38f5bc6456dc6
Dongkyun-Jang <jangdongkyun6576@gmail.com>
2022-05-21 13:11:08 +0900

    :recycle: 띄어쓰기 수정

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -5,9 +5,9 @@ import { reset } from './reset';
 export const GlobalStyle = createGlobalStyle`
    @font-face {
       font-family: 'Pretendard';
-        font-style: normal;
-        src: url(https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css) format('woff2'),
-        url(https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css) format('woff');
+      font-style: normal;
+      src: url(https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css) format('woff2'),
+         url(https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css) format('woff');
    }
 
    ${reset}
