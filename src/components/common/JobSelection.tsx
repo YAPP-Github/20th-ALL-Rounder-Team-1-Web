0bc159821391cf1447502dc5ea14934493167079
이수동 <tnehd1998@gmail.com>
2022-05-26 22:47:11 +0900

    :sparkles: JobSelection 컴포넌트 추가

---
+++ src/components/common/JobSelection.tsx
@@ -0,0 +1,40 @@
+import { Dispatch, SetStateAction } from 'react';
+import styled from 'styled-components';
+
+interface JobSelectionProps {
+  name: string;
+  isClicked: boolean;
+  setIsClicked: Dispatch<SetStateAction<boolean>>;
+}
+
+export const JobSelection = ({ name, isClicked, setIsClicked }: JobSelectionProps) => {
+  const onClick = () => {
+    return setIsClicked(!isClicked);
+  };
+
+  return (
+    <>
+      {isClicked ? (
+        <Selected onClick={onClick}>{name}</Selected>
+      ) : (
+        <NotSelected onClick={onClick}>{name}</NotSelected>
+      )}
+    </>
+  );
+};
+
+const Selected = styled.button`
+  background-color: #0000ff;
+  border: 1px solid #0000ff;
+  color: #ffffff;
+  padding: 10px 20px;
+  border-radius: 20px;
+`;
+
+const NotSelected = styled.button`
+  background-color: #ffffff;
+  border: 1px solid #0000ff;
+  color: #0000ff;
+  padding: 10px 20px;
+  border-radius: 20px;
+`;
