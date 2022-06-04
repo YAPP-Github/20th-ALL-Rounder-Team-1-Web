4e7fee1703c9e03b2fa55a4acfebf6b0d74eb6c9
이수동 <tnehd1998@gmail.com>
2022-06-04 23:16:07 +0900

    :sparkles: 페이지 lazy 요소 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,4 @@
-export * from './Home';
-export * from './Login';
+import { lazy } from 'react';
+
+export const Home = lazy(() => import('./Home'));
+export const Login = lazy(() => import('./Login'));
