82224ae2051e492a5d04e9e4b2b5cf4cec1d3d1f
이수동 <tnehd1998@gmail.com>
2022-06-04 23:11:42 +0900

    :sparkles: 페이지 템플릿 생성

---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import { PageLayout } from '@/components';
+
+const Login = () => {
+  return (
+    <PageLayout title="로그인 페이지">
+      <h1>로그인 페이지</h1>
+    </PageLayout>
+  );
+};
+
+export default Login;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,2 @@
-export * from '.';
+export * from './Home';
+export * from './Login';
