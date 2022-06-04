76a6621ca8e56795388e92d8f4a76ec33daf7a08
이수동 <tnehd1998@gmail.com>
2022-06-04 23:52:21 +0900

    :recycle: 테스트용 요소 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
-import { Link } from 'react-router-dom';
 import { PageLayout } from '@/components';
 
 const Home = () => {
   return (
     <PageLayout title="홈 페이지">
       <h1>홈 페이지</h1>
-      <Link to="/login">Login</Link>
     </PageLayout>
   );
 };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,9 @@
-import { Link } from 'react-router-dom';
 import { PageLayout } from '@/components';
 
 const Login = () => {
   return (
     <PageLayout title="로그인 페이지">
       <h1>로그인 페이지</h1>
-      <Link to="/">Home</Link>
     </PageLayout>
   );
 };
