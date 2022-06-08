1e3519da618fb81c3927ddeff10f7ce3e09b240d
Soo Dong Lee <72953316+tnehd1998@users.noreply.github.com>
2022-06-09 00:04:18 +0900

    [Weekand-1035] JobSelection 컴포넌트 디자인에 맞게 수정 (#77)
    * :recycle: 외부 배열에 클릭한 요소들을 저장하는 형식으로 수정
    
    * :recycle: 가독성을 위해 JobSelection에서 Interest로 수정
    
    * :art: 디자인에 맞게 css 수정

---
+++ src/components/common/Interest.tsx
@@ -0,0 +1,52 @@
+import { Dispatch, SetStateAction, useState } from 'react';
+import styled from 'styled-components';
+
+interface InterestProps {
+  name: string;
+  totalChoices: string[];
+  setTotalChoices: Dispatch<SetStateAction<string[]>>;
+}
+
+export const Interest = ({ name, totalChoices, setTotalChoices }: InterestProps) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const onClick = () => {
+    if (isClicked) {
+      const exceptClickedName = totalChoices.filter((totalChoice) => totalChoice !== name);
+      setTotalChoices(exceptClickedName);
+    } else {
+      setTotalChoices([...totalChoices, name]);
+    }
+    setIsClicked(!isClicked);
+  };
+
+  return (
+    <>
+      {isClicked ? (
+        <Selected onClick={onClick}>{name}</Selected>
+      ) : (
+        <NotSelected onClick={onClick}>{name}</NotSelected>
+      )}
+    </>
+  );
+};
+
+const Selected = styled.button`
+  background-color: ${({ theme: { colors } }) => colors.WeekandBlueSub};
+  color: ${({ theme: { colors } }) => colors.WeekandBlue};
+  padding: 8px 16px;
+  border-radius: 108px;
+  margin: 0px 5px;
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+const NotSelected = styled.button`
+  background-color: ${({ theme: { colors } }) => colors.Gray100};
+  color: ${({ theme: { colors } }) => colors.Gray400};
+  padding: 8px 16px;
+  border-radius: 108px;
+  margin: 0px 5px;
+  font-size: 16px;
+  font-weight: 500;
+`;
