bf1846a44df9887d63fcf54ced1db15e0f9b55df
이수동 <tnehd1998@gmail.com>
2022-06-10 22:04:55 +0900

    :sparkles: FriendStories 버튼에 맞는 애니메이션 추가

--- src/components/common/FriendStories.tsx
+++ src/components/common/FriendStories.tsx
@@ -11,26 +11,71 @@ export const FriendStories = () => {
     { name: 'Corey', imgUrl: '' },
     { name: 'Trae', imgUrl: '' },
     { name: 'James', imgUrl: '' },
-    { name: '손흥민', imgUrl: '' },
-    { name: '손흥민', imgUrl: '' },
-    { name: '손흥민', imgUrl: '' },
-    { name: '손흥민', imgUrl: '' },
-    { name: '손흥민', imgUrl: '' },
+    { name: '1', imgUrl: '' },
+    { name: '2', imgUrl: '' },
+    { name: '3', imgUrl: '' },
+    { name: '4', imgUrl: '' },
+    { name: '5', imgUrl: '' },
+    { name: '6', imgUrl: '' },
+    { name: '7', imgUrl: '' },
+    { name: '8', imgUrl: '' },
+    { name: '9', imgUrl: '' },
+    { name: '10', imgUrl: '' },
   ];
 
   const [start, setStart] = useState(0);
-  const [end, setEnd] = useState(6);
+  const endX = -(64 + 60 * friends.length + 26 * (friends.length - 1)) + 640;
+
+  const showLeftButton = () => {
+    if (start === 0) {
+      return false;
+    }
+    return true;
+  };
+
+  const showRightButton = () => {
+    if (start === endX) {
+      return false;
+    }
+    return true;
+  };
+
+  const onClickLeft = () => {
+    if (start + 360 > 0) {
+      setStart(0);
+    } else {
+      setStart(start + 360);
+    }
+  };
+
+  const onClickRight = () => {
+    if (start - 360 < endX) {
+      setStart(endX);
+    } else {
+      setStart(start - 360);
+    }
+  };
 
   return (
-    <Wrapper>
-      {friends.map((friend) => (
-        <FriendStory name={friend.name} imgUrl={friend.imgUrl} />
+    <Wrapper currentLoc={start}>
+      {friends.map((friend, index) => (
+        <FriendStory key={index} name={friend.name} imgUrl={friend.imgUrl} />
       ))}
+      {showLeftButton() && (
+        <Button onClick={onClickLeft} className="left">
+          Left
+        </Button>
+      )}
+      {showRightButton() && (
+        <Button onClick={onClickRight} className="right">
+          Right
+        </Button>
+      )}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.ul`
+const Wrapper = styled.ul<{ currentLoc: number }>`
   width: 576px;
   height: 162px;
   display: flex;
@@ -39,4 +84,21 @@ const Wrapper = styled.ul`
   padding: 0px 32px;
   border-radius: 10px;
   background-color: greenyellow;
+  overflow: hidden;
+  position: relative;
+
+  li:nth-child(1) {
+    transition: all 0.6s ease-in;
+    margin-left: ${({ currentLoc }) => `${currentLoc}px`};
+  }
+`;
+
+const Button = styled.div`
+  position: absolute;
+  &.left {
+    left: 24px;
+  }
+  &.right {
+    right: 24px;
+  }
 `;
