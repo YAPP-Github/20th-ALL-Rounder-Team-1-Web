99b1da268f97524d4c62d8b99de511efadb997c1
Dongkyun-Jang <jangdongkyun6576@gmail.com>
2022-06-11 15:11:28 +0900

    :sparkles: 비밀번호 찾기 페이지 연결

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { PopUpPortal } from './PopUpPortal';
 import { DimmedLayer, PopUp } from '@/components';
-import { Home, Login } from '@/pages';
+import { Home, Login, FindPassword } from '@/pages';
 
 const App = () => {
   return (
@@ -23,6 +23,14 @@ const App = () => {
           element={
             <Suspense fallback={<>Loading...</>}>
               <Login />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/find-password"
+          element={
+            <Suspense fallback={<>Loading...</>}>
+              <FindPassword />
             </Suspense>
           }
         />
