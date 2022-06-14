f0685f046dd8b19956b3b681b81557377f90ef21
Soo Dong Lee <72953316+tnehd1998@users.noreply.github.com>
2022-06-14 22:31:16 +0900

    [Weekand-1040] 보여지는 팝업 요소 context로 전환되게 수정 (#90)
    * :sparkles: 팝업 요소를 바꾸는 원리로 수정
    
    * :recycle: 쓰이지 않는 정보 제거
    
    * :recycle: 보여지는 초기화면 Login으로 수정

--- src/components/common/PopUp.tsx
+++ src/components/common/PopUp.tsx
@@ -1,18 +1,14 @@
 import { useContext } from 'react';
 import styled from 'styled-components';
 
-import { CalendarPopUp, FindPasswordPopUp } from '@/components';
 import { PopUpContext } from '@/contexts';
 
 export const PopUp = () => {
-  const { isCalendarOpen, isFindPasswordOpen, isVisible } = useContext(PopUpContext);
+  const { isVisible, innerContent } = useContext(PopUpContext);
 
   return (
     <div>
-      <PopUpWrapper visible={isVisible}>
-        {isCalendarOpen && <CalendarPopUp />}
-        {isFindPasswordOpen && <FindPasswordPopUp />}
-      </PopUpWrapper>
+      <PopUpWrapper visible={isVisible}>{innerContent}</PopUpWrapper>
     </div>
   );
 };
