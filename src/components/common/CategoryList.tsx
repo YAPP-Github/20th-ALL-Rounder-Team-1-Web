9053b1dfb9beb77a6b1d9736aec9ae8cf13efeac
이수동 <tnehd1998@gmail.com>
2022-06-18 13:31:29 +0900

    :sparkles: 오른쪽 마우스 클릭 시 이벤트 추가

--- src/components/common/CategoryList.tsx
+++ src/components/common/CategoryList.tsx
@@ -2,10 +2,15 @@ import styled from 'styled-components';
 
 import { getCategories } from '@/utils';
 import { Category } from './Category';
+import { useCallback, useEffect, useState } from 'react';
+import { useContextMenu } from '@/hooks';
 
 const categories = getCategories();
 
 export const CategoryList = () => {
+  const { pointX, pointY, show } = useContextMenu();
+  const [showSorting, setShowSorting] = useState(false);
+
   return (
     <Wrapper>
       {categories.map((category) => (
@@ -16,6 +21,22 @@ export const CategoryList = () => {
           content={category.content}
         />
       ))}
+      {show && (
+        <ContextMenu pointX={pointX} pointY={pointY}>
+          <p>새 카테고리</p>
+          <p onMouseOver={() => setShowSorting(true)} onMouseLeave={() => setShowSorting(false)}>
+            정렬
+          </p>
+          {showSorting && (
+            <ul onMouseOver={() => setShowSorting(true)} onMouseLeave={() => setShowSorting(false)}>
+              <li>최신순</li>
+              <li>오래된순</li>
+              <li>오름차순</li>
+              <li>내림차순</li>
+            </ul>
+          )}
+        </ContextMenu>
+      )}
     </Wrapper>
   );
 };
@@ -25,4 +46,17 @@ const Wrapper = styled.div`
   grid-template-columns: repeat(3, 356px);
   gap: 32px;
   align-items: center;
+`;
+
+const ContextMenu = styled.div<{ pointX: number; pointY: number }>`
+  width: 138px;
+  height: 92px;
+  position: absolute;
+  border-radius: 10px;
+  background-color: #fff;
+  left: ${({ pointX }) => `${pointX}px`};
+  top: ${({ pointY }) => `${pointY}px`};
+
+  p {
+  }
 `;
---
+++ src/hooks/useContextMenu.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useCallback, useState } from 'react';
+
+export const useContextMenu = () => {
+  const [pointX, setPointX] = useState(0);
+  const [pointY, setPointY] = useState(0);
+  const [show, setShow] = useState(false);
+
+  const handleContextMenu = useCallback(
+    (event: MouseEvent) => {
+      event.preventDefault();
+      setPointX(event.pageX);
+      setPointY(event.pageY);
+      setShow(true);
+    },
+    [setShow, setPointX, setPointY]
+  );
+
+  const handleClick = useCallback(() => (show ? setShow(false) : null), [show]);
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+    document.addEventListener('contextmenu', handleContextMenu);
+    return () => {
+      document.removeEventListener('click', handleClick);
+      document.removeEventListener('contextmenu', handleContextMenu);
+    };
+  });
+
+  return { pointX, pointY, show };
+};
