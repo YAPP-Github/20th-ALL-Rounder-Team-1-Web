c8a2f5635b317082a9ae31c8ff27a4a7cb65280b
이수동 <tnehd1998@gmail.com>
2022-06-18 14:11:32 +0900

    :art: Context 메뉴 css 디자인에 맞게 수정

--- src/components/common/CategoryList.tsx
+++ src/components/common/CategoryList.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 import { getCategories } from '@/utils';
 import { Category } from './Category';
-import { useCallback, useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useContextMenu } from '@/hooks';
 
 const categories = getCategories();
@@ -23,17 +23,26 @@ export const CategoryList = () => {
       ))}
       {show && (
         <ContextMenu pointX={pointX} pointY={pointY}>
-          <p>새 카테고리</p>
-          <p onMouseOver={() => setShowSorting(true)} onMouseLeave={() => setShowSorting(false)}>
-            정렬
-          </p>
+          <Menu>새 카테고리</Menu>
+          <SortMenu
+            onMouseOver={() => setShowSorting(true)}
+            onMouseLeave={() => setShowSorting(false)}
+          >
+            <p>정렬</p>
+            <img src="../../assets/setting_right_button.png" alt="" />
+          </SortMenu>
           {showSorting && (
-            <ul onMouseOver={() => setShowSorting(true)} onMouseLeave={() => setShowSorting(false)}>
+            <SortingMenu
+              pointX={pointX}
+              pointY={pointY}
+              onMouseOver={() => setShowSorting(true)}
+              onMouseLeave={() => setShowSorting(false)}
+            >
               <li>최신순</li>
               <li>오래된순</li>
               <li>오름차순</li>
               <li>내림차순</li>
-            </ul>
+            </SortingMenu>
           )}
         </ContextMenu>
       )}
@@ -56,7 +65,67 @@ const ContextMenu = styled.div<{ pointX: number; pointY: number }>`
   background-color: #fff;
   left: ${({ pointX }) => `${pointX}px`};
   top: ${({ pointY }) => `${pointY}px`};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: start;
+`;
 
-  p {
+const Menu = styled.p`
+  height: 26px;
+  color: ${({ theme: { colors } }) => colors.Gray700};
+  ${({ theme: { fonts } }) => fonts.Body1}
+  padding: 10px 20px;
+
+  &:hover {
+    background-color: ${({ theme: { colors } }) => colors.Gray100};
+  }
+
+  &:hover:first-child {
+    border-radius: 10px 10px 0px 0px;
+  }
+
+  &:hover:nth-child(2) {
+    border-radius: 0px 0px 10px 10px;
+  }
+`;
+
+const SortMenu = styled(Menu)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  img {
+    width: 6.55px;
+    height: 11.15px;
+  }
+`;
+
+const SortingMenu = styled.ul<{ pointX: number; pointY: number }>`
+  position: absolute;
+  left: 138px;
+  top: 46px;
+  width: 138px;
+  height: 184px;
+  background-color: #fff;
+  border-radius: 10px;
+
+  li {
+    height: 26px;
+    color: ${({ theme: { colors } }) => colors.Gray700};
+    ${({ theme: { fonts } }) => fonts.Body1}
+    padding: 10px 20px;
+
+    &:hover {
+      background-color: ${({ theme: { colors } }) => colors.Gray100};
+    }
+
+    &:hover:first-child {
+      border-radius: 10px 10px 0px 0px;
+    }
+
+    &:hover:last-child {
+      border-radius: 0px 0px 10px 10px;
+    }
   }
 `;
