bcfd177465787abd0e5d7dad77c71783366536c8
이수동 <tnehd1998@gmail.com>
2022-06-21 12:20:38 +0900

    :art: EditProfile 디자인에 맞게 css 제작

--- src/pages/setting/components/EditProfile.tsx
+++ src/pages/setting/components/EditProfile.tsx
@@ -3,14 +3,15 @@ import styled from 'styled-components';
 
 import { ProfileInfo } from './ProfileInfo';
 
-import { Interest } from '@/common';
+import { Button, Interest } from '@/common';
 import { INTERESTS, JOBS } from '@/utils';
 
 export const EditProfile = () => {
   const userProfile = {
-    email: 'example@gmail.com',
-    imageUrl: '',
-    nickName: '김아무개',
+    email: 'giannis@gmail.com',
+    imageUrl:
+      'https://fadeawayworld.net/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTgyMDU2NzMyMTM3MzY2NjU5/giannis.jpg',
+    nickName: 'Greek Freak',
     purpose: 'We can do, Weekand!',
     jobs: ['취준생', '프리랜서'],
     interests: ['N잡', '사업', '자기계발'],
@@ -23,39 +24,99 @@ export const EditProfile = () => {
 
   return (
     <>
-      <div>
+      <ImageAndEmail>
         <img src={imageUrl} alt="User Image" />
         <div>
           <p>{email}</p>
           <button>프로필 사진 바꾸기</button>
         </div>
-      </div>
+      </ImageAndEmail>
       <ProfileInfo title="닉네임" content={nickName} />
       <ProfileInfo title="한줄목표" content={purpose} />
-      {/* <ProfileInfo title="직업" content={totalJobs} />
-      <ProfileInfo title="관심사" content={totalInterests} /> */}
-      <div>
+      <ProfileInfo title="직업" content={totalJobs.join(', ')} />
+      <SelectInterests>
         <p>초기화</p>
-        {JOBS.map((job) => (
-          <Interest
-            className="select_interest"
-            name={job}
-            totalChoices={totalJobs}
-            setTotalChoices={setTotalJobs}
-          />
-        ))}
-      </div>
-      <div>
+        <div>
+          {JOBS.map((job) => (
+            <Interest
+              className="setting_interest"
+              name={job}
+              totalChoices={totalJobs}
+              setTotalChoices={setTotalJobs}
+            />
+          ))}
+        </div>
+      </SelectInterests>
+      <ProfileInfo title="관심사" content={totalInterests.join(', ')} />
+      <SelectInterests>
         <p>초기화</p>
-        {INTERESTS.map((interest) => (
-          <Interest
-            className="select_interest"
-            name={interest}
-            totalChoices={totalInterests}
-            setTotalChoices={setTotalInterests}
-          />
-        ))}
-      </div>
+        <div>
+          {INTERESTS.map((interest) => (
+            <Interest
+              className="setting_interest"
+              name={interest}
+              totalChoices={totalInterests}
+              setTotalChoices={setTotalInterests}
+            />
+          ))}
+        </div>
+      </SelectInterests>
+      <ButtonWrapper>
+        <Button className="question_button" onClick={() => console.log('확인')}>
+          확인
+        </Button>
+      </ButtonWrapper>
     </>
   );
 };
+
+const ImageAndEmail = styled.div`
+  display: flex;
+
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 24px;
+    background-color: ${({ theme: { colors } }) => colors.Gray300};
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: flex-start;
+    margin-left: 34px;
+    p {
+      color: ${({ theme: { colors } }) => colors.Gray900};
+      ${({ theme: { fonts } }) => fonts.Body1}
+    }
+
+    button {
+      color: ${({ theme: { colors } }) => colors.WeekandBlue};
+      ${({ theme: { fonts } }) => fonts.SubHead2}
+    }
+  }
+`;
+
+const SelectInterests = styled.div`
+  display: flex;
+  margin: 8px 0px;
+
+  p {
+    width: 83px;
+    text-align: end;
+    margin-top: 4px;
+    margin-right: 32px;
+    color: ${({ theme: { colors } }) => colors.WeekandBlue};
+    ${({ theme: { fonts } }) => fonts.Body3}
+  }
+
+  div {
+    width: 580px;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  margin-left: 112px;
+  margin-top: 28px;
+`;
