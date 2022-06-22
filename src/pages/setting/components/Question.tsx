78ba77156a254bc0ca34fc2891ba90f679dae021
이수동 <tnehd1998@gmail.com>
2022-06-22 13:53:29 +0900

    :art: Question 제목 수정

--- src/pages/setting/components/Question.tsx
+++ src/pages/setting/components/Question.tsx
@@ -5,10 +5,7 @@ import { Button } from '@/common';
 export const Question = () => {
   return (
     <Wrapper>
-      <Title>
-        <h1>문의하실 내용을</h1>
-        <h1>작성해주세요 :)</h1>
-      </Title>
+      <Title>문의하실 내용을 작성해주세요 :)</Title>
       <Description>문의에 대한 답변은 로그인된 이메일로 전송됩니다.</Description>
       <TextArea placeholder="내용을 입력해주세요."></TextArea>
       <Button className="question_button" onClick={() => console.log('문의하기')}>
@@ -22,12 +19,13 @@ const Wrapper = styled.div`
   padding: 20px 0px;
 `;
 
-const Title = styled.div`
-  h1 {
-    font-size: 24px;
-    font-weight: 700;
-    line-height: 36px;
-  }
+const Title = styled.h1`
+  width: 157px;
+  height: 72px;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 36px;
+  color: ${({ theme: { colors } }) => colors.Gray900};
 `;
 
 const Description = styled.p`
