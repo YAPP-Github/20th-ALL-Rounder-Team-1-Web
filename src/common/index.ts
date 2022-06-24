2322ee78cb8f29e770570b5898fbc86b00298a17
Soo Dong Lee <72953316+tnehd1998@users.noreply.github.com>
2022-06-24 21:25:06 +0900

    [Weekand-1057] 설정 페이지 : 프로필 편집 (#119)
    * :recycle: Interest 컴포넌트 프로필에서도 사용할 수 있도록 수정
    
    * :recycle: 서버 구축 전 임시 데이터 형식 변경
    
    * :sparkles: 프로필 요소를 나타내는 ProfileInfo 컴포넌트 생성
    
    * :art: EditProfile 디자인에 맞게 css 제작
    
    * :sparkles: 이미 선택된 관심사 선택되도록 지정
    
    * :sparkles: 초기화 버튼 클릭 시 선택된 요소 모두 초기화되도록 적용
    
    * :art: 디자인과 다른 비율 수정
    
    * :recycle: 약속한 컨벤션에 맞게 수정
    
    * :art: css 요소 수정
    
    * :art: Question 제목 수정
    
    * :recycle: 관심사 판단 여부 방법 수정
    
    * :sparkles: 닉네임, 한줄 목표 수정 가능하도록 변경
    
    * :sparkles: 편집에 따른 토스트 메시지 추가
    
    * :art: 토스트 메시지 화면 아래에 고정
    
    * :recycle: 토스트 메세지 요소 제거

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,6 +1,7 @@
 export * from './Button';
 export * from './DebounceInput';
 export * from './Input';
+export * from './Interest';
 export * from './PopUp';
 export * from './layout';
 export * from './calender';
--- src/pages/setting/components/EditProfile.tsx
+++ src/pages/setting/components/EditProfile.tsx
@@ -1,7 +1,163 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+import { ProfileInfo } from '.';
+
+import { Button, Interest } from '@/common';
+import { INTERESTS, JOBS } from '@/utils';
+
 export const EditProfile = () => {
+  const userProfile = {
+    email: 'giannis@gmail.com',
+    imageUrl:
+      'https://fadeawayworld.net/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTgyMDU2NzMyMTM3MzY2NjU5/giannis.jpg',
+    nickName: 'Greek Freak',
+    purpose: 'We can do, Weekand!',
+    jobs: ['취준생', '프리랜서'],
+    interests: ['N잡', '사업', '자기계발'],
+  };
+
+  const { email, imageUrl, nickName, purpose, jobs, interests } = userProfile;
+
+  const [currentNickname, setCurrentNickname] = useState(nickName);
+  const [currentPurpose, setCurrentPurpose] = useState(purpose);
+  const [totalJobs, setTotalJobs] = useState<string[]>([...jobs]);
+  const [totalInterests, setTotalInterests] = useState<string[]>([...interests]);
+
+  const isSelectedInterest = (name: string, totalSelected: string[]) => {
+    return totalSelected.includes(name);
+  };
+
+  const onClickReset = (type: string) => {
+    if (type === 'JOBS') {
+      return setTotalJobs([]);
+    }
+    if (type === 'INTERESTS') {
+      return setTotalInterests([]);
+    }
+
+    return;
+  };
+
   return (
-    <>
-      <h1>프로필 편집</h1>
-    </>
+    <Wrapper>
+      <ImageAndEmail>
+        <img src={imageUrl} alt="User Image" width={80} height={80} />
+        <div>
+          <p className="email">{email}</p>
+          <button className="edit_profile_img" onClick={() => console.log('프로필 편집 팝업')}>
+            프로필 사진 바꾸기
+          </button>
+        </div>
+      </ImageAndEmail>
+      <ProfileInfo
+        title="닉네임"
+        content={currentNickname}
+        changeContent={setCurrentNickname}
+        isInput={true}
+      />
+      <ProfileInfo
+        title="한줄목표"
+        content={currentPurpose}
+        changeContent={setCurrentPurpose}
+        isInput={true}
+      />
+      <ProfileInfo title="직업" content={totalJobs.join(', ')} />
+      <SelectInterests>
+        <button className="reset" onClick={() => onClickReset('JOBS')}>
+          초기화
+        </button>
+        <div className="interest_list">
+          {JOBS.map((job, index) => (
+            <Interest
+              key={index}
+              className="setting_interest"
+              name={job}
+              totalChoices={totalJobs}
+              setTotalChoices={setTotalJobs}
+              isChosen={isSelectedInterest(job, totalJobs)}
+            />
+          ))}
+        </div>
+      </SelectInterests>
+      <ProfileInfo title="관심사" content={totalInterests.join(', ')} />
+      <SelectInterests>
+        <button className="reset" onClick={() => onClickReset('INTERESTS')}>
+          초기화
+        </button>
+        <div className="interest_list">
+          {INTERESTS.map((interest, index) => (
+            <Interest
+              key={index}
+              className="setting_interest"
+              name={interest}
+              totalChoices={totalInterests}
+              setTotalChoices={setTotalInterests}
+              isChosen={isSelectedInterest(interest, totalInterests)}
+            />
+          ))}
+        </div>
+      </SelectInterests>
+      <ButtonWrapper>
+        <Button className="question_button" onClick={() => console.log('변경 버튼 클릭')}>
+          확인
+        </Button>
+      </ButtonWrapper>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  padding: 0px 20px;
+`;
+
+const ImageAndEmail = styled.div`
+  display: flex;
+
+  img {
+    border-radius: 24px;
+    background-color: ${({ theme: { colors } }) => colors.Gray300};
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: flex-start;
+    margin-left: 34px;
+
+    .email {
+      color: ${({ theme: { colors } }) => colors.Gray900};
+      ${({ theme: { fonts } }) => fonts.Body1}
+    }
+
+    .edit_profile_img {
+      color: ${({ theme: { colors } }) => colors.WeekandBlue};
+      ${({ theme: { fonts } }) => fonts.SubHead2}
+    }
+  }
+`;
+
+const SelectInterests = styled.div`
+  display: flex;
+  align-items: flex-start;
+  margin: 8px 0px;
+
+  .reset {
+    width: 83px;
+    text-align: end;
+    margin-top: 8px;
+    margin-right: 32px;
+    color: ${({ theme: { colors } }) => colors.WeekandBlue};
+    ${({ theme: { fonts } }) => fonts.Body3}
+  }
+
+  .interest_list {
+    width: 580px;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  margin-left: 112px;
+  margin-top: 28px;
+`;
