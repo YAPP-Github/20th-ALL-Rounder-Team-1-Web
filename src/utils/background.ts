1959b4715a129fc3d21915f5a740587b1d15cc91
Dongkyun-Jang <jangdongkyun6576@gmail.com>
2022-06-25 03:31:04 +0900

    :recycle: 유틸함수에서 커스텀 훅으로 변경

--- src/utils/background.ts
+++
@@ -1,9 +0,0 @@
-export const handleBackgroundColor = () => {
-  const whitePages = ['/login', '/find-password', '/register', '/select-interest'];
-
-  if (whitePages.includes(location.pathname)) {
-    return (document.body.style.backgroundColor = '#fff');
-  }
-
-  document.body.style.backgroundColor = '#F5F7F8';
-};
