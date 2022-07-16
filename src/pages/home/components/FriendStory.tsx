fb32806b59cf9d1b95d35696e1af45e28310825c
이수동 <tnehd1998@gmail.com>
2022-07-16 16:03:30 +0900

    style: 친구 스토리 이름 text-overflow 적용

--- src/pages/home/components/FriendStory.tsx
+++ src/pages/home/components/FriendStory.tsx
@@ -33,6 +33,10 @@ export const FriendStory = ({
 const Wrapper = styled.li`
   width: 60px;
   height: 90px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
 `;
 
 const Image = styled.div<{ imgUrl?: string }>`
@@ -57,8 +61,11 @@ const Border = styled.div<{ isSelected: boolean }>`
 const Name = styled.h1`
   font-size: 14px;
   font-weight: 500;
+  width: 49px;
   line-height: 22.4px;
   text-align: center;
-  margin-top: 8px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
   color: ${({ theme: { colors } }) => colors.Gray600};
 `;
--- src/utils/dummyData.ts
+++ src/utils/dummyData.ts
@@ -86,47 +86,45 @@ export const INTERESTS = [
   '여행',
 ];
 
-export const getFriends = () => {
-  return [
-    { id: 0, name: '빌리', imgUrl: '' },
-    {
-      id: 1,
-      name: '손흥민',
-      imgUrl: 'https://img.khan.co.kr/news/2022/04/08/l_2022040901000999500089741.jpg',
-    },
-    {
-      id: 2,
-      name: 'Morant',
-      imgUrl:
-        'https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTg5MzY3Mzc1ODIzMzgyNDk2/ja-morant-sneaks-among-the-mvp-candidates.jpg',
-    },
-    { id: 3, name: 'Cody', imgUrl: '' },
-    { id: 4, name: 'Woody', imgUrl: '' },
-    { id: 5, name: 'Corey', imgUrl: '' },
-    { id: 6, name: 'Trae', imgUrl: '' },
-    { id: 7, name: 'James', imgUrl: '' },
-    { id: 8, name: '1', imgUrl: '' },
-    { id: 9, name: '2', imgUrl: '' },
-    { id: 10, name: '3', imgUrl: '' },
-    { id: 11, name: '4', imgUrl: '' },
-    {
-      id: 12,
-      name: 'Son',
-      imgUrl:
-        'https://w.namu.la/s/18cdc3e45fee5e76f3f3a29056396db782c12502afe39de7539f82893cd2339c70522a6bdbb849a2cf990ce554dca7db8307eac97b5591f4a623fc595694af5157adfb48ccdda9c8bbba83c73ff58ca7b74e3351d818772ba0f8d4aaad3d2f6f0273faf1fc9b0e5ada654b3f5b7ed6f6',
-    },
-    {
-      id: 13,
-      name: '모란트',
-      imgUrl:
-        'https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTg5MzY3Mzc1ODIzMzgyNDk2/ja-morant-sneaks-among-the-mvp-candidates.jpg',
-    },
-    { id: 14, name: '7', imgUrl: '' },
-    { id: 15, name: '8', imgUrl: '' },
-    { id: 16, name: '9', imgUrl: '' },
-    { id: 17, name: '10', imgUrl: '' },
-  ];
-};
+export const FRIENDS = [
+  { id: 0, name: '빌리리리리리리리리리리리리리리리리리', imgUrl: '' },
+  {
+    id: 1,
+    name: '손흥민',
+    imgUrl: 'https://img.khan.co.kr/news/2022/04/08/l_2022040901000999500089741.jpg',
+  },
+  {
+    id: 2,
+    name: 'Morant',
+    imgUrl:
+      'https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTg5MzY3Mzc1ODIzMzgyNDk2/ja-morant-sneaks-among-the-mvp-candidates.jpg',
+  },
+  { id: 3, name: 'Cody', imgUrl: '' },
+  { id: 4, name: 'Woody', imgUrl: '' },
+  { id: 5, name: 'Corey', imgUrl: '' },
+  { id: 6, name: 'Trae', imgUrl: '' },
+  { id: 7, name: 'James', imgUrl: '' },
+  { id: 8, name: '1', imgUrl: '' },
+  { id: 9, name: '2', imgUrl: '' },
+  { id: 10, name: '3', imgUrl: '' },
+  { id: 11, name: '4', imgUrl: '' },
+  {
+    id: 12,
+    name: 'Son',
+    imgUrl:
+      'https://w.namu.la/s/18cdc3e45fee5e76f3f3a29056396db782c12502afe39de7539f82893cd2339c70522a6bdbb849a2cf990ce554dca7db8307eac97b5591f4a623fc595694af5157adfb48ccdda9c8bbba83c73ff58ca7b74e3351d818772ba0f8d4aaad3d2f6f0273faf1fc9b0e5ada654b3f5b7ed6f6',
+  },
+  {
+    id: 13,
+    name: '모란트',
+    imgUrl:
+      'https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTg5MzY3Mzc1ODIzMzgyNDk2/ja-morant-sneaks-among-the-mvp-candidates.jpg',
+  },
+  { id: 14, name: '7', imgUrl: '' },
+  { id: 15, name: '8', imgUrl: '' },
+  { id: 16, name: '9', imgUrl: '' },
+  { id: 17, name: '10', imgUrl: '' },
+];
 
 export const CATEGORIES = [
   {
