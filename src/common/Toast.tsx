07bb4eda965a8c9adb58959d262836af2846aa41
Dongkyun-Jang <jangdongkyun6576@gmail.com>
2022-07-24 15:57:07 +0900

    :sparkles: 토스트 관련 코드 개선

--- src/common/Toast.tsx
+++ src/common/Toast.tsx
@@ -1,29 +1,38 @@
 import { useContext } from 'react';
+import cn from 'classnames';
 import styled from 'styled-components';
 
 import { ToastContext } from '@/contexts';
 
 export const Toast = () => {
-  const { isVisible, text, isSuccess } = useContext(ToastContext);
+  const { getToast } = useContext(ToastContext);
+
+  const { isToasted, toastType, toastMessage } = getToast();
 
   return (
-    <Wrapper visible={isVisible} isSuccess={isSuccess}>
-      <p className="content">{text}</p>
+    <Wrapper className={cn(toastType, isToasted && 'open')}>
+      <p className="content">{toastMessage}</p>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div<{ visible: boolean; isSuccess: boolean }>`
-  display: ${({ visible }) => (visible ? 'flex' : 'none')};
-  justify-content: center;
-  align-items: center;
+const Wrapper = styled.div`
+  display: none;
   position: fixed;
+  text-align: center;
   width: 100%;
-  height: 48px;
   left: 0px;
   bottom: 0px;
-  background-color: ${({ isSuccess, theme: { colors } }) =>
-    isSuccess ? colors.WeekandBlue : colors.WeekandRed};
+  background-color: ${({ theme: { colors } }) => colors.WeekandBlue};
+  padding: 11px 0;
+
+  &.open {
+    display: block;
+  }
+
+  &.error {
+    background-color: ${({ theme: { colors } }) => colors.WeekandRed};
+  }
 
   p.content {
     color: #fff;
