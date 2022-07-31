ff05dee31e7f82154349767336684391593f1c4a
Soo Dong Lee <72953316+tnehd1998@users.noreply.github.com>
2022-07-31 12:27:20 +0900

    [Weekand-1098] 알람 목록 api 연결 (#196)
    * :sparkles: 알람 메세지 데이터 연결
    
    * :art: 알람이 더 위에 표시되게 수정

--- src/common/AlarmList.tsx
+++ src/common/AlarmList.tsx
@@ -1,29 +1,57 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { Alarm } from '.';
 
+import { useNotifications } from '@/api/notification';
 import { ALARMS } from '@/utils';
 
+interface IAlarm {
+  id: string;
+  message: string;
+  type: string;
+}
+
 export const AlarmList = () => {
+  const [alarms, setAlarms] = useState<IAlarm[]>();
+  const { notifications } = useNotifications();
+
+  const showNofications = async () => {
+    const {
+      data: {
+        notifications: { notifications: alarms },
+      },
+    } = await notifications({
+      variables: {
+        page: 0,
+        size: 10,
+      },
+    });
+    setAlarms(alarms);
+  };
+
+  useEffect(() => {
+    showNofications();
+  }, []);
+
   return (
     <Wrapper>
-      {ALARMS.map(({ id, type, content }) => (
-        <Alarm key={id} type={type} content={content} />
-      ))}
+      {alarms &&
+        alarms.map(({ id, type, message }) => <Alarm key={id} type={type} content={message} />)}
     </Wrapper>
   );
 };
 
 const Wrapper = styled.ul`
   width: 420px;
-  height: 500px;
+  height: 515px;
   position: absolute;
-  top: 76px;
-  left: 832px;
+  top: 66px;
+  right: 268px;
   padding: 24px;
   border-radius: 6px;
   background-color: #fff;
-  z-index: 2;
+  z-index: 21;
   box-shadow: 5.320000171661377px 7.980000019073486px 26.600000381469727px 0px #0000000f;
   overflow-y: auto;
   &::-webkit-scrollbar {
